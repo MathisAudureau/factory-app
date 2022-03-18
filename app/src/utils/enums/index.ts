6b0808af1b51b98b9e9d00e1f2ed9c56fc85d601
MathisDevs <mathisaud.pro@gmail.com>
2022-03-19 00:45:14 +0100

    Add utils folder

---
+++ app/src/utils/enums/index.ts
@@ -0,0 +1,5 @@
+export enum ColorsPanel {
+    YES = "#16A34A",
+    ERROR = "#DC2626",
+    INVISIBLE = "#2f3136"
+}
---
+++ app/src/utils/types/index.ts
@@ -0,0 +1,6 @@
+import {MessageButton, MessageEmbed, MessageSelectMenu} from "discord.js";
+
+export type Embed = MessageEmbed
+export type Button = MessageButton
+export type Menu = MessageSelectMenu
+
